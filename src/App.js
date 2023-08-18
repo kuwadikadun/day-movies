45c17375db42a418b2650b98ed4834b54f867d35
kuwadikadun <kuwadikadun@gmail.com>
2023-08-18 23:09:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
 
-function App() {
+const App = () => {
+  const [popularMovie, setPopularMovie] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovie(result);
+    });
+  }, []);
+
+  const PopularMovieList = () => {
+    return popularMovie.map((movie, i) => {
+      return (
+        <div className="movie-wrapper" key={i}>
+          <div className="movie-title">{movie.title}</div>
+          <img
+            className="movie-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+          />
+          <div className="movie-date">Release: {movie.release_date}</div>
+          <div className="movie-rating">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovie(query.results);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>DAY MOVIE MANTAP</h1>
+        <input
+          type="text"
+          className="movie-search"
+          placeholder="Cari film kesukaan anda..."
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="movie-container">
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
